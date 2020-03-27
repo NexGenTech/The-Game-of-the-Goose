cb0ae685fd455955ba6e329d3f79c7e5a1eb49e5
Vaggelis <vaggelis987654321@gmail.com>
2020-03-27 19:10:04 +0200

    updatin the inventory .c

--- inventory.c
+++ inventory.c
@@ -6,3 +6,30 @@
  *@date 27/03/2020
  *
  */
+
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "inventory.h"
+
+  typedef struct _Inventory {
+    Set* objects;
+    int max_objects;
+  }Inventory;
+
+  Inventory* invenotry_create() {
+    Inventory* newInventory = NULL;
+
+    newInventory = (Inventory*) malloc(sizeof(Inventory));
+
+    if (newInventory == NULL) {
+      return NULL;
+    }
+
+    newInventory->objects = set_create();
+
+    newInventory->max_objects = MAX_ITEMS;
+
+    return newInventory;
+  }
+
+  
