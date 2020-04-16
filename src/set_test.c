83304de247cf2ba9a04a9777006597bddcb9007e
Vaggelis <vaggelis987654321@gmail.com>
2020-04-16 16:09:33 +0300

    Fixed the identation

--- src/set_test.c
+++ src/set_test.c
@@ -12,40 +12,40 @@
 
 int main(void){
 
-	printf("This is the start of the set_test\n");
+  printf("This is the start of the set_test\n");
 
 
-	Set* set = set_create();
+  Set* set = set_create();
 
-	int i;
+  int i;
 
-	for(i=1 ; i<=10 ; i++){		//Add to the set every Id from 1 to 10
-		set_add(set, i);
-	}
+  for(i=1 ; i<=10 ; i++){      //Add to the set every Id from 1 to 10
+    set_add(set, i);
+  }
 
-	printf("Tis is the image of the set before any deletes\n");
-	set_print(set);
+  printf("Tis is the image of the set before any deletes\n");
+  set_print(set);
 
-	for(i=1 ; i<=10 ; i++){
-		if(i%2 == 0) {
-			set_del(set, i);
-			printf("This is the image of the set after deleting Id %d\n",i);
-			set_print(set);
-		}
-	}
+  for(i=1 ; i<=10 ; i++){
+    if(i%2 == 0) {
+      set_del(set, i);
+      printf("This is the image of the set after deleting Id %d\n",i);
+      set_print(set);
+    }
+  }
 
-	set_print(set);
+  set_print(set);
 
-	for(i=1 ; i<=10 ; i++){
-		if(set_search_id(set,i) == NO_ID){
-			printf("The id %d does not exist in the set\n", i);
-		} else {
-			printf("The id %d exists in the set\n", i);
-		}
-	}
+  for(i=1 ; i<=10 ; i++){
+    if(set_search_id(set,i) == NO_ID){
+      printf("The id %d does not exist in the set\n", i);
+    } else {
+      printf("The id %d exists in the set\n", i);
+    }
+  }
 
-	set_destroy(set);
+  set_destroy(set);
 
-	printf("This is the end of the set_test\n");
-	return(0);
+  printf("This is the end of the set_test\n");
+  return(0);
 }
